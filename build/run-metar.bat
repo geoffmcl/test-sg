3d50424ef979ff00b18f161c5d3e66467c4a6922
Geoff McLane <ubuntu@geoffair.info>
2016-06-13 20:04:00 +0200

    Add metar app, and CURL deps

---
+++ build/run-metar.bat
@@ -0,0 +1,21 @@
+@setlocal
+@set TMPEXE=Release\metar.exe
+@if NOT EXIST %TMPEXE% goto NOEXE
+@set TMPBIN=D:\FG\d-and-c\3rdParty.x64\bin
+@if NOT EXIST %TMPBIN%\nul goto NOBIN
+
+@set PATH=%TMPBIN%;%PATH%
+
+%TMPEXE% %*
+
+@goto END
+
+:NOEXE
+@echo Can NOT locate exe %TMPEXE%! *** FIX ME ***
+@goto END
+
+:NOBIN
+@echo Can NOT locate bin %TMPBIN%! *** FIX ME ***
+@goto END
+
+:END
