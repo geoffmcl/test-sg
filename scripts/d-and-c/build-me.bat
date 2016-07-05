5092dc42c947df54c61bc864f0c5cd8e6905d091
Geoff McLane <ubuntu@geoffair.info>
2016-07-05 19:52:18 +0200

    Add a Qt5_DIR check - now close to new dependency!

--- scripts/d-and-c/build-me.bat
+++ scripts/d-and-c/build-me.bat
@@ -1,1 +1,11 @@
+@if "%Qt5_DIR%x" == "x" goto WARN
+@echo Have environment Qt5_DIR=%Qt5_DIR%
+@goto DOIT
+:WARN
+@echo NOTE: Qt5_DIR **NOT** set in environment...
+@pause
+:DOIT
+@set TMPTBGN=%TIME%
 call d-and-c.x64 %* simgear flightgear
+@call elapsed %TMPTBGN%
+@set TMPTBGN=
