59d982dcd0c5749f341dc8de328941967da18089
Geoff McLane <ubuntu@geoffair.info>
2017-02-18 19:05:56 +0100

    run-metar.bat - switch to using X: drive 3rdParty.x64 bin

--- build/run-metar.bat
+++ build/run-metar.bat
@@ -1,7 +1,8 @@
 @setlocal
 @set TMPEXE=Release\metar.exe
 @if NOT EXIST %TMPEXE% goto NOEXE
-@set TMPBIN=D:\FG\d-and-c\3rdParty.x64\bin
+@set TMPBIN=X:\3rdParty.x64\bin
+@REM set TMPBIN=D:\FG\d-and-c\3rdParty.x64\bin
 @if NOT EXIST %TMPBIN%\nul goto NOBIN
 
 @set PATH=%TMPBIN%;%PATH%
